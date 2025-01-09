8f4b3622dfa605474517ae11c0e35784ac067a81
Bryan Lam <bryanl156@nycstudents.net>
2025-01-09 12:11:36 -0500

    Create constructor, getSpecialName, getSpecial, getSpecialMax, and setSpecial

--- Farmer.java
+++ Farmer.java
@@ -1,3 +1,26 @@
 public class Farmer extends Adventurer{
+  private int berries, berriesMax;
   
+
+  public Farmer(String name, int berries){
+    super(name, 30);
+    this.berries = berries;
+    berriesMax = 64;
+  }
+
+  public String getSpecialName(){
+    return "Farmer " + getName().substring(0, 2);
+  }
+
+  public int getSpecial(){
+    return berries;
+  }
+
+  public int getSpecialMax(){
+    return berriesMax;
+  }
+
+  public void setSpecial(int newBerry){
+    berries = newBerry;
+  }
 }
