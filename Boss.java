ee50cfd87cefee8503cfcdaec8189c2ed90cc9df
Sean Takahashi <seanyuto@gmail.com>
2025-01-13 10:54:04 -0500

    edited boss class to be a God character, wrote up to support method

--- Boss.java
+++ Boss.java
@@ -1,73 +1,68 @@
 public class Boss extends Adventurer{
-/* EDIT NUMERICAL VALUES, MESSAGES
+    private int godpts, godptsMax;
 
-    private int , Max;
-
-    public Boss(String name, int){
-        super(name, );  
-        this. = ;
-        Max = ; 
+    public Boss(String name, int hp){
+        super(name, hp);
+        godptsMax = 500;
+        this.godpts = godptsMax / 2;
       }
-    
+
       public String getSpecialName(){
         return "Boss " + getName().substring(0, 2);
       }
-    
+
       public int getSpecial(){
-        return ;
+        return godpts;
       }
-    
+
       public int getSpecialMax(){
-        return Max;
+        return godptsMax;
       }
-    
-      public void setSpecial(int ){
-        __ = __;
+
+      public void setSpecial(int newgodpts){
+        godpts = newgodpts;
       }
-    
+
       public String attack(Adventurer other){
-        if (getSpecial() >= 4){ 
-          other.applyDamage(7); 
-          applyDamage(1); 
-          setSpecial(getSpecial() - 4); 
-          return other.getSpecialName() + " has been ________.";
+        if (getSpecial() >= 10){
+          other.applyDamage(15);
+          applyDamage(3);
+          setSpecial(getSpecial() - 10);
+          return other.getSpecialName() + " has been punished by God and sucked 15 HP.";
         }else{
-          applyDamage(8); 
-          return getSpecialName() + " _______ and instead attacked themself!";
+          applyDamage(8);
+          return getSpecialName() + " was out of God points and he crashed out! He endured 8 HP of damage.";
         }
       }
-    
+
       public String support(){
-        setSpecial(getSpecial() + 10); 
-        setHP(getHP() + 5); 
-        return getSpecialName() + " ______________.";
+        setSpecial(getSpecial() + 10);
+        setHP(getHP() + 10);
+        return getSpecialName() + " got 10 God points and healed 10 HP.";
       }
-    
+
+/*
       public String support(Adventurer other){
-        if (getSpecial() >= 5){ 
-          if (other.getSpecialName().substring(0, 4).equals("Boss")){
-            other.setSpecial(other.getSpecial() + 5); 
-            setSpecial(getSpecial() - 5); 
-            return "_____________.";
-          }else{
-            setSpecial(getSpecial() - 4); 
-            other.setHP(other.getHP() + 10); 
+        if (getSpecial() >= 5){
+            setSpecial(getSpecial() - 4);
+            other.setHP(other.getHP() + 10);
             return other.getSpecialName() + " was healed.";
           }
         }else{
           return "_____________.";
         }
       }
-    
+*/
+
       public String specialAttack(Adventurer other){
-        if (getSpecial() >= 10){ 
-          other.applyDamage(15); 
-          setSpecial(getSpecial() - 10); 
+        if (getSpecial() >= 10){
+          other.applyDamage(15);
+          setSpecial(getSpecial() - 10);
           applyDamage(3);
           return other.getSpecialName() + " _______________.";
         }else{
           return "_______________________________.";
         }
       }
-   */ 
+
 }
