4ce657528403b19a78544e3e9755e87bd14b4ab2
Sean Takahashi <seanyuto@gmail.com>
2025-01-14 11:01:23 -0500

    tried debugging methods, need to keep debugging textbox and drawparty

--- Tester.java
+++ Tester.java
@@ -1,9 +1,23 @@
+import java.util.*;
+
 public class Tester{
   public static void main(String[] args){
+    ArrayList<Adventurer> party = new ArrayList<Adventurer>();
+
+
     Adventurer a1 = Game.createRandomAdventurer("BOB");
-    System.out.println(a1.getSpecialName());
-    System.out.println(a1.getHP());
+    Adventurer a2 = Game.createRandomAdventurer("REBECCA");
+    Adventurer a3 = Game.createRandomAdventurer("JAMES");
+
+    party.add(a1); party.add(a2); party.add(a3);
 
     Game.drawBackground();
+
+
+/* METHODS THAT NEED DEBUGGING
+
+    Game.TextBox(10, 75, 30, 80, "hello world")
+    Game.drawParty(party, 10);
+*/
   }
 }
