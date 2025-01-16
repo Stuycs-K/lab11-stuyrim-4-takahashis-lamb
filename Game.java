677bb622e095bd053b244a02ac11257fb4442a9e
Sean Takahashi <seanyuto@gmail.com>
2025-01-16 11:01:35 -0500

    changed variable to represent correct amount of people on party, corrected syntax errors

--- Game.java
+++ Game.java
@@ -271,15 +271,15 @@ public class Game{
         if(move.equals("attack") || move.equals("a")){
           /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
           //YOUR CODE HERE
-          party.get(whichPlayer).attack(enemies.get(enemychoice));
-          drawmove(" attacked ", party.get(whichPlayer), enemies.get(enemychoice));
+          party.get(whichPlayer).attack(enemies.get(personchoice));
+          drawmove(" attacked ", party.get(whichPlayer), enemies.get(personchoice));
           /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
         }
         else if(move.equals("special") || move.equals("sp")){
           /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
           //YOUR CODE HERE
-          party.get(whichPlayer).specialAttack(enemies.get(enemychoice));
-          drawmove(" used special attack on ", party.get(whichPlayer), enemies.get(enemychoice));
+          party.get(whichPlayer).specialAttack(enemies.get(personchoice));
+          drawmove(" used special attack on ", party.get(whichPlayer), enemies.get(personchoice));
           /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
         }
         else if(move.startsWith("su ") || move.startsWith("support ")){
@@ -288,6 +288,7 @@ public class Game{
           /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
           //YOUR CODE HERE
           party.get(whichPlayer).support(party.get(personchoice));
+          drawmove(" supported ", party.get(whichPlayer), party.get(personchoice));
           /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
         }
 
@@ -319,19 +320,23 @@ public class Game{
         //Enemy action choices go here!
         /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
         //YOUR CODE HERE
-        int randperson = (int)(Math.random() * 3);
+        int randperson = (int)(Math.random() * party.size());
         int randmove = (int)(Math.random() * 8);
         if(randmove <= 4){ //increases chance of normal attack, not sustainable to have same chance of regular and special attack
           //implement attack
           enemies.get(whichOpponent).attack(party.get(randperson));
+          drawmove(" attacked ", enemies.get(whichOpponent), party.get(randperson));
         }
         else if(randmove == 5 || randmove == 6){
           //implement special
           enemies.get(whichOpponent).specialAttack(party.get(randperson));
+          drawmove(" used their special attack on ", enemies.get(whichOpponent), party.get(randperson));
         }
         else{
           //implement support
-          enemies.get(whichOpponent).support(enemies.get(randperson));
+          int randsupport = (int)(Math.random() * enemies.size());
+          enemies.get(whichOpponent).support(enemies.get(randsupport));
+          drawmove(" supported ", enemies.get(whichOpponent), enemies.get(randsupport));
         }
 
         /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
